288ad60889332c0528fbb6d8697f479385b57fd8
Hong <hoang.bui49@gmail.com>
2023-11-28 14:25:59 -0500

    Completed tip calculator

--- script.js
+++ script.js
@@ -2,11 +2,39 @@ let bill = 0
 let partysize = 0
 
 function calculateTip(tipPercent) {
-    bill = document.querySelector('.billAmount').value
-    partysize = document.querySelector('.partySize').value
-    let tip = bill * (5/100)
+    bill = Number(document.querySelector('.billAmount').value)
+    partysize = Number(document.querySelector('.partySize').value)
+    let tip = Number(bill * (tipPercent/100))
     let total = bill + tip
     console.log(total)
+    console.log(tip)
     let tipPerPerson = tip / partysize
     let totalPerPerson = total / partysize
+    document.querySelector('.tipnumber').innerHTML = `$${tipPerPerson}`
+    document.querySelector('.totalnumber').innerHTML = `$${totalPerPerson}`
 }
+
+document.querySelector('#one').addEventListener('click', () => {
+    calculateTip(5)
+})
+
+document.querySelector('#two').addEventListener('click', () => {
+    calculateTip(10)
+})
+
+document.querySelector('#three').addEventListener('click', () => {
+    calculateTip(15)
+})
+
+document.querySelector('#four').addEventListener('click', () => {
+    calculateTip(25)
+})
+
+document.querySelector('#five').addEventListener('click', () => {
+    calculateTip(50)
+})
+
+document.querySelector('#six').addEventListener('click', () => {
+    custom = prompt('What percent tip?')
+    calculateTip(custom)
+})
